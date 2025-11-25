51cfe2334b7a59717666fa8b3fc606051a9d7c0c
Aswanthcreator <aswanthshaji2009@gmail.com>
2025-11-26 02:07:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,15 +10,16 @@ API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 # List of channel/group IDs to monitor
 # Example in .env:  CHAT_IDS=-1001111111111,-1002222222222
-CHAT_IDS = list(map(int, os.getenv("CHAT_IDS", "").split(",")))
+CHAT_IDS = list(map(int, os.getenv("CHAT_IDS", "-1002168048982").split(",")))
 
 # Duration (in seconds) for each chat_id before auto-delete
 # Example in .env:  ID_DUR=-1001111111111:30,-1002222222222:180
 ID_DUR = {}
-raw_dur = os.getenv("ID_DUR", "")
+raw_dur = os.getenv("ID_DUR", "-1002168048982:7200")
 
 if raw_dur:
     for pair in raw_dur.split(","):
         chat_id, duration = pair.split(":")
         ID_DUR[int(chat_id)] = int(duration)
+
 
