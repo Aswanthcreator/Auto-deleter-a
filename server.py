57da7be1d320366fdd652a6d4480fc371c26d1b1
Aswanthcreator <aswanthshaji2009@gmail.com>
2025-11-26 07:12:40 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,13 @@
+import os
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"status": "Bot running"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
